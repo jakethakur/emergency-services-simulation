762d4a6f8587b81bb23d0f4ee97706da2c3e8bb8
jakethakur22 <34597857+jakethakur22@users.noreply.github.com>
2018-05-09 19:33:32 +0100

    commented and optimised code

--- ambulance.m
+++ ambulance.m
@@ -1,102 +1,121 @@
-% fill array with empty values for program runtime
-X = zeros (12000, 1);
-Y = zeros (12000, 1);
+simulationLength = 12000; % length of simulation in "minutes"
 
 % set position of hospital (where ambulance will return to)
-hospitalPositionX = 80;
-hospitalPositionY = 80;
+hospitalPositionX = 0;
+hospitalPositionY = 0;
 
-Dist = zeros();
-Dist2 = zeros();
-Dist_Random = zeros();
-Dist_Nonrandom = zeros();
-X(1, 1) = hospitalPositionX; % array of x values
-Y(1, 1) = hospitalPositionY; % array of y values
+
+Scale = 100; % size of area calls can come in from (multiplied by 2 by the program for positive and negative)
+Prob = 0.1; % probability (out of 1) of an incoming call every hour
+
+% Don't change any of the below variables
+
+% Dist = zeros();
+% Dist2 = zeros();
+
+% x and y positions of random walk ambulance
+X = zeros (simulationLength, 1);
+Y = zeros (simulationLength, 1);
+
+Dist_Random = zeros(); % distance of random ambulance movement
+Dist_Nonrandom = zeros(); % distance of non-random ambulance movement
+
+X(1, 1) = hospitalPositionX; % array of x values (for random ambulance)
+Y(1, 1) = hospitalPositionY; % array of y values (for non-random ambulance)
+
 Xvalue = hospitalPositionX; % current x value (starting position)
 Yvalue = hospitalPositionY; % current y value (")
-clock = 0;
-N = 1;
-Scale = 100; % size of populated area
-Prob = 0.1; % probability of an incoming call every hour
 
-for T = 2 : length(X)
+callNumber = 1; % current call being dealt with
+
+
+
+for Timer = 2 : length(X) % for loop to repeat simulation
  
-    R = rand;
-    RN = round (5 * R);
+    random = rand; % generate random number
+    random = floor(5 * random); % generate a random number between 0 and 4 to decide direction of random walk (or 0 for no random walk)
+    
+    % expect no movement (will be overwritten if there is)
+    X(Timer, 1) = X(Timer - 1, 1);
+    Y(Timer, 1) = Y(Timer - 1, 1);
+    
+    % random walk
  
-    if RN == 1
-        if Xvalue > Scale
-            X(T, 1) = X(T - 1, 1);
-        else
+    if random == 1 % move right
+        if Xvalue > Scale % check the ambulance is not out of bounds
+            X(Timer, 1) = X(Timer - 1, 1);
+        else % move
             Xvalue = Xvalue + 1;
-            X(T, 1) = Xvalue;
+            X(Timer, 1) = Xvalue; % push current x value to array of x values
         end
-    else X(T, 1) = X(T - 1, 1);
-    end
- 
-    if RN == 2
-        if Xvalue < - Scale
-            X(T, 1) = X(T - 1, 1);
+        
+    elseif random == 2 % move left
+        if Xvalue < -Scale
+            X(Timer, 1) = X(Timer - 1, 1);
         else
             Xvalue = Xvalue - 1;
-            X(T, 1) = Xvalue;
+            X(Timer, 1) = Xvalue;
         end
-    else X(T, 1) = X(T - 1, 1);
-    end
-    if RN == 3
+    
+    elseif random == 3 % move up
         if Yvalue > Scale
-            Y(T, 1) = Y(T - 1, 1);
+            Y(Timer, 1) = Y(Timer - 1, 1);
         else
             Yvalue = Yvalue + 1;
-            Y(T, 1) = Yvalue;
+            Y(Timer, 1) = Yvalue;
         end
-    else Y(T, 1) = Y(T - 1, 1);
-    end
-    if RN == 4
-        if Yvalue < - Scale
-            Y(T, 1) = Y(T - 1, 1);
+        
+    elseif random == 4 % move down
+        if Yvalue < -Scale
+            Y(Timer, 1) = Y(Timer - 1, 1);
         else
             Yvalue = Yvalue - 1;
-            Y(T, 1) = Yvalue;
+            Y(Timer, 1) = Yvalue;
         end
-    else Y(T, 1) = Y(T - 1, 1);
     end
-    clock = clock + 1;
-    if clock == 61
-        r = binornd(1, Prob);
-        if r > R
-            position (1, 1) = (rand - .5) * 2 * Scale;
-            position (1, 2) = (rand - .5) * 2 * Scale;
-            X(T, 1) = position (1, 1);
-            Y(T, 1) = position (1, 2);
-            Dist_Random(N, 1) = sqrt((position(1, 1) - X(T - 1, 1)) .^ 2 + (position(1, 2) - Y(T - 1, 1)) .^ 2);
-            Dist_Nonrandom(N, 1) = sqrt((position(1, 1) - 0) .^ 2 + (position(1, 2) - 0) .^ 2);
-            N = N + 1;
-            T = T + 1;
-            X(T, 1) = 0;
-            Y(T, 1) = 0;
-            T = T + 1;
-         
-            % return to hospital
+    
+    if mod(Timer,60) == 1 % check if a new hour has elapsed
+        r = binornd(1, Prob); % binomial distribution to check if a call has been received (1 = number of trials)
+        if r > rand % ???
+            % random call position within scale
+            position (1, 1) = (rand - 0.5) * 2 * Scale; % random x position within scale
+            position (1, 2) = (rand - 0.5) * 2 * Scale; % random y position within scale
+            % ambulance moves instantly to call position
+            X(Timer, 1) = position (1, 1); 
+            Y(Timer, 1) = position (1, 2);
+            % find distance that both ambulance types have moved to reach this call
+            Dist_Random(callNumber, 1) = sqrt((position(1, 1) - X(Timer - 1, 1)) .^ 2 + (position(1, 2) - Y(Timer - 1, 1)) .^ 2);
+            Dist_Nonrandom(callNumber, 1) = sqrt((position(1, 1) - 0) .^ 2 + (position(1, 2) - 0) .^ 2);
+            
+            callNumber = callNumber + 1;
+            
+            % return ambulance to hospital
+            Timer = Timer + 1;
             Xvalue = hospitalPositionX;
             Yvalue = hospitalPositionY;
-         
+            X(Timer, 1) = Xvalue;
+            Y(Timer, 1) = Yvalue;
         end
-        clock = 0;
     end
- 
 end
 
-X;
-Y;
-comet (X, Y);
+% plot graphs
+
+% comet plot (visulisation of random ambulance moving)
+ax = axes;
+set(ax,'xlim',[-Scale Scale],'ylim',[-Scale Scale]); % set axes
+hold (ax);
+comet (ax, X, Y);
 position = zeros (1, 2);
+
+% random and nonrandom distances plotted against each other
 figure;
 plot(Dist_Random(:, 1));
 hold on;
 plot(Dist_Nonrandom(:, 1));
-legend('Dist_Random', 'Dist_Nonrandom');
+legend('random distance', 'nonrandom distance');
 
+% print information to console
 fprintf('random distance: %f.\n',mean(Dist_Random));
 fprintf('nonrandom distance: %f.\n',mean(Dist_Nonrandom));
 
